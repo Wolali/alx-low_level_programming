ac39616c1e79afb09ddb72b735b1409ea42d6ed3
Wolali <wolaliasafo@gmail.com>
2023-02-17 16:18:24 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alpha in lower and uppercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+
+	putchar("\n");
+}
