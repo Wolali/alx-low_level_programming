1948eaa15dccd5cb4b899446bca3fc8fbc2c4cc5
Wolali <wolaliasafo@gmail.com>
2023-02-22 12:13:04 -0800

    last digit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Computes absolute value of an integer
+ * @c - The number to be computer
+ * Return: Absolute value of the number or zero
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+	return (c);
+	else
+	return (-c);
+}
