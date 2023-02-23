cf9169b9edf4e7af9673ff6e6d5baccc519f4a70
Wolali <wolaliasafo@gmail.com>
2023-02-22 16:46:38 -0800

    first 98 Fibonacci

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,18 +2,20 @@
 
 /**
  * _abs - Computes absolute value of an integer
- * @c - The number to be computer
+ * @i - The number to be computed
  * Return: Absolute value of the number or zero
  */
 
-int _abs(int c)
+int _abs(int i)
 {
-	if (c < 0)
-	{
-	int abs_val;
+	int i;
 
-	abs_val = c * -1;
-	return (abs_val);
+	if (i >= 0)
+	{
+		return (i);
 	}
-	return (c);
+	else
+	{
+		return (i * -1);
+	}
 }
