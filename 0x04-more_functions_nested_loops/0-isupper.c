a4f1f825b4da86d97266c86bacb79164ddc1222f
Wolali <wolaliasafo@gmail.com>
2023-02-23 15:19:46 +0000

    More nested

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase characters
+ * @c: The number to be checked
+ * Return: 1 for the uppercase or 0 for anything else
+ */
+
+int _isupper(int c)
+{
+	if (x >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
