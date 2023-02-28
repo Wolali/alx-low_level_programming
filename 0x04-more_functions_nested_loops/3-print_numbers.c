215c3008fb9d6acbcdb1b193b9d4353c62137120
Wolali <wolaliasafo@gmail.com>
2023-02-28 11:59:46 -0800

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Checks for a digit
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
